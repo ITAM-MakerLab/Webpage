aadc50285c4d329f6ef3e2a37a816ac7869a4c62
downvec <downvec@gmail.com>
2018-02-15 09:25:19 -0600

    Created models for Post and Tutorial. Light visual changes to index page. Clean build

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    tituloPost = models.TextField()
+    autorPost = models.CharField(max_length=140)
+    contenidoPost = models.TextField()
+
+    def __str__(self):
+        return self.tituloPost + " por " + self.autorPost
+
+
+class Tutorial(models.Model):
+    tituloTut = models.TextField()
+    autorTut = models.CharField(max_length=140)
+    contenido = models.TextField()
+
+    def __str__(self):
+        return self.tituloTut + " por " + self.autorTut
