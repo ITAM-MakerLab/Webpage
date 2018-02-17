ec108b18b0143a64f99f95e319f105179c78b678
Alonso Martinez <alonso.mtz99@gmail.com>
2018-02-17 13:10:07 -0600

    Cleaned visuals. Added MathJax

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Post(models.Model):
     tituloPost = models.TextField()
@@ -19,3 +22,10 @@ class Tutorial(models.Model):
 
     def __str__(self):
         return self.tituloTut + " por " + self.autorTut
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to="blog/static/blog/images/")
+    smallBio = models.TextField()
+    credentials = models.CharField(max_length=100)
